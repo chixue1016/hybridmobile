07f702183aef04dc13d5f3ec3a8d251b754961f7
wangzhaod <wangzhaod@yonyou.com>
2015-12-16 20:50:53 +0800

    界面切换时自适应高度和登陆界面禁止切换
    Change-Id: I3c5fd71bec286b244a74f7af7f8b87eb74b76d15
    Signed-off-by: wangzhaod <wangzhaod@yonyou.com>

--- echarts_mavenweb/src/main/webapp/js/failure.slider.js
+++ echarts_mavenweb/src/main/webapp/js/failure.slider.js
@@ -5,20 +5,38 @@ function FailureSlider() {
 	var _detailSlider	= 2;
 	var _sliderSpeed	= 100; // ms
 
-	var _mySwiper 		= new Swiper( '.swiper-container', {
-	    loop: false,
-		autoplay: false,
-	});
+	var _mySwiper;
 
-	this.toDetail		= function() {
-		_mySwiper.slideTo( _loginSlider, _sliderSpeed, false);
+	function init() {
+		_mySwiper 		= new Swiper( '.swiper-container', {
+			noSwiping 	: true, // 如果slide上增加了类swiper-no-swiping，则无法滑动。		
+		    autoHeight	: true,
+		    loop		: false,
+			autoplay	: false,
+
+			onInit 		: function( swiper ){
+		  		swiper.container[0].style.height = swiper.slides[swiper.activeIndex].offsetHeight+'px';
+           	},
+
+			onSlideChangeEnd	: function( swiper ){
+                swiper.container[0].style.height = swiper.slides[swiper.activeIndex].offsetHeight+'px';
+            }			
+		});	
+		
+	}
+
+	this.toLogin		= function() {
+		_mySwiper.slideTo( _loginSlider, _sliderSpeed, true); // true 代表触发 onSliderChange 回调函数 
 	};
-	
+
 	this.toSummary		= function() {
-		_mySwiper.slideTo( _summarySlider, _sliderSpeed, false);
+		_mySwiper.slideTo( _summarySlider, _sliderSpeed, true);
 	};
 
 	this.toDetail		= function() {
-		_mySwiper.slideTo( _detailSlider, _sliderSpeed, false);
+		_mySwiper.slideTo( _detailSlider, _sliderSpeed, true);
 	};
+
+	// Constructor Code.
+	init();
 }
