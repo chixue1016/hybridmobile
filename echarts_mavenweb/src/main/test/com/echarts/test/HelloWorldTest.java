e2df3ae556888f35191fbe7e47e987fc79fa062e
wangzhaod <wangzhaod@yonyou.com>
2015-12-25 20:29:38 +0800

    test转移目录
    Change-Id: I533214646f95252f5bb63e354a058d7afa88bcad
    Signed-off-by: wangzhaod <wangzhaod@yonyou.com>

---
+++ echarts_mavenweb/src/main/test/com/echarts/test/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.echarts.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testHelloWorld() {
+		assertEquals("Hello world", "Hello world");
+	}
+
+}
--- echarts_mavenweb/src/main/test/com/echarts/test/LoadDataControllerTest.java
+++ echarts_mavenweb/src/main/test/com/echarts/test/LoadDataControllerTest.java
@@ -1,6 +1,6 @@
 package com.echarts.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
